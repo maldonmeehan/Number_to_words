507d6ab3c48941b8dea65489e91298b70d447a10
Maldon Meehan and Matt Wilkin <matt@>
2016-06-28 16:56:37 -0700

    establish next steps to establish functionality for teens and triple + digit numbers.

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -4,17 +4,31 @@ class String
     two_digit = {10 => "ten", 20 => "twenty", 30 => "thirty", 40 => "fourty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety"}
 
     # teens_digit = {11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eightteen", 19 => "nineteen"}
+    #tripple_digit (to be made)
+
     # numbers_to_words_array = []
     # self.split()
     #
+
     #
     if self.length() == 1
       integer_input = self.to_i()
       single_digit.fetch(integer_input)
 
-    elsif self.length() ==2
+    elsif self.length() ==2 #need statment to say and not teens
       integer_input = self.to_i()
       two_digit.fetch(integer_input)
+      # two_digit + single_digit = numbers_to_words_array
+
+      #elsif statment for teens
+      #integer_input = self.to_i()
+      #teen_digit.fetch(integer_input)
+
+      #elsif self.length() ==3
+      #integer_input = self.to_i()
+      #tripple_digit.fetch(integer_input)
+      # tripple_digit + two_digit + single_digit = numbers_to_words_array
+
     end
   end
 end
