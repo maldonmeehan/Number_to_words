db8469f710a95d73e90799390d6e40f60ce26a56
Maldon Meehan and Matt Wilkin <matt@>
2016-06-28 16:34:58 -0700

    establish functionality for two digit numbers.

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,7 +1,20 @@
-class Fixnum
+class String
   define_method(:numbers_to_words) do
     single_digit = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
+    two_digit = {10 => "ten", 20 => "twenty", 30 => "thirty", 40 => "fourty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety"}
 
-    single_digit.fetch(1)
+    # teens_digit = {11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eightteen", 19 => "nineteen"}
+    # numbers_to_words_array = []
+    # self.split()
+    #
+    #
+    if self.length() == 1
+      integer_input = self.to_i()
+      single_digit.fetch(integer_input)
+
+    elsif self.length() ==2
+      integer_input = self.to_i()
+      two_digit.fetch(integer_input)
+    end
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -3,6 +3,14 @@ require('numbers_to_words.rb')
 
 describe('Fixnum#numbers_to_words') do
   it("translates numbers in numeric form into written words") do
-    expect((1).numbers_to_words()).to(eq("one"))
+    expect(("1").numbers_to_words()).to(eq("one"))
   end
+
+  it("translates numbers that are two digits ending in 0 into written words") do
+    expect(("10").numbers_to_words()).to(eq("ten"))
+  end
+  #
+  # it("translates numbers between 11 and 19 into equivelent written words") do
+  #   expect((11).numbers_to_words()).to(eq("eleven"))
+  # end
 end
